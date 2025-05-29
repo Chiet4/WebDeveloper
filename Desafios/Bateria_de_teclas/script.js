2554644842453cffd55e881aeae59b5e60623ec4
Anchieta Albano <111232477+Chiet4@users.noreply.github.com>
2025-05-29 10:43:17 -0300

    feat: add funcão de play sound

--- Desafios/Bateria_de_teclas/script.js
+++ Desafios/Bateria_de_teclas/script.js
@@ -4,4 +4,16 @@ document.body.addEventListener("keyup", (event) => {
 
 function tocarSound(sound) {
   let audioElement = document.querySelector(`#s_${sound}`);
+  let keyElement = document.querySelector(`div[data-key=${sound}]`);
+  if (audioElement) {
+    audioElement.currentTime = 0;
+    audioElement.play();
+  }
+  if (keyElement) {
+    keyElement.classList.add("active");
+
+    setTimeout(() => {
+      keyElement.classList.remove("active");
+    }, 300);
+  }
 }
